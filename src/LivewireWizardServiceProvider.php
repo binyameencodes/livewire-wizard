3f2b9a0111039ebb01fdb43f4644a4e31c260260
binyameencodes <37224249+binyameencodes@users.noreply.github.com>
2022-09-29 14:46:58 +0500

    Update LivewireWizardServiceProvider.php

--- src/LivewireWizardServiceProvider.php
+++ src/LivewireWizardServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Vildanbina\LivewireWizard;
+namespace Binyameencodes\LivewireWizard;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
